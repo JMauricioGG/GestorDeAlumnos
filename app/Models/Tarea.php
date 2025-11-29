848c6461cd84b312304183ccc5a869634bc84ffd
mauri <juan.gomez0292@alumnos.uddg.mx>
2025-11-28 18:05:09 -0600

    Tarea Controller y Tarea Request

--- app/Models/Tarea.php
+++ app/Models/Tarea.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tarea extends Model
 {
-    //
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'fecha_entrega',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Tarea;
 
 class User extends Authenticatable
 {
@@ -45,4 +46,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+public function tareas()
+{
+    return $this->hasMany(Tarea::class);
+}
+
+
 }
